86de1d56d8ca82449b41f9ff08f031c5b7026f2b
Satish Reddy Modugu <smodugu@unomaha.edu>
2019-08-22 13:39:59 -0500

    First Commit

---
+++ src/main/java/com/satish/iot/api/devices/DeviceDiscoverServiceApplication.java
@@ -0,0 +1,15 @@
+package com.satish.iot.api.devices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DeviceDiscoverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceDiscoverServiceApplication.class, args);
+	}
+
+}
